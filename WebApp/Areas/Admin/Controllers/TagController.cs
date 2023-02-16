6aed277f1c338a018b44bc13ea740ce65c9bd0ee
MrAzimzada <mehemmedezimzase@gmail.com>
2023-02-16 19:29:14 +0400

    Finish Tag create update delete

--- WebApp/Areas/Admin/Controllers/TagController.cs
+++ WebApp/Areas/Admin/Controllers/TagController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebApp.Data;
+using WebApp.Models;
 
 namespace WebApp.Areas.Admin.Controllers
 {
@@ -12,16 +14,76 @@ namespace WebApp.Areas.Admin.Controllers
     public class TagController : Controller
     {
         private readonly ILogger<TagController> _logger;
+        private readonly AppDbContext _context;
 
-        public TagController(ILogger<TagController> logger)
+        public TagController(ILogger<TagController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var tag = _context.Tags.ToList();
+
+            return View(tag);
+        }
+        public IActionResult Create()
+        {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Tag tag)
+        {
+            var findTag = _context.Tags.FirstOrDefault(x => x.TagName == tag.TagName);
+            if (findTag != null)
+            {
+                ViewBag.Tagexist = "Ay qaqa bunann yazmisan !!! ";
+                return View();
+            }
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var delete = _context.Tags.FirstOrDefault(x => x.Id == id);
+            return View(delete);
+        }
+        [HttpPost]
+        public IActionResult Delete(Tag tag)
+        {
+            _context.Tags.Remove(tag);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var edit = _context.Tags.FirstOrDefault(x=>x.Id == id);
+            return View(edit);
+        }
+        [HttpPost]
+        public IActionResult Edit(Tag tag)
+        {
+            var findTag = _context.Tags.FirstOrDefault(x => x.TagName == tag.TagName);
+            if (findTag != null)
+            {
+                ViewBag.Tagexist = "Bu categorya zaten var Agzina geleni yazma!!!!";
+                return View(findTag);
+            }
+            _context.Tags.Update(tag);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
