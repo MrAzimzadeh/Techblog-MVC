42c7092c462c67ddb5157b00cfc6fb8d737b7485
MrAzimzada <mehemmedezimzase@gmail.com>
2023-03-02 12:45:02 +0400

    Add Comments Article

--- WebApp/Controllers/ArticleController.cs
+++ WebApp/Controllers/ArticleController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebApp.Data;
 using WebApp.Models;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
@@ -29,10 +31,66 @@ namespace WebApp.Controllers
         {
             if (id == null)
                 return NotFound();
-            var article = _context.Articles.Include(x => x.User).Include(x => x.Category).Include(x => x.ArticleTags).ThenInclude(x => x.Tag).Include(x => x.Comments).ThenInclude(x => x.User).FirstOrDefault(x => x.Id == id);
+            var article = _context.Articles
+            .Include(x => x.User)
+            .Include(x => x.Category)
+            .Include(x => x.ArticleTags)
+            .ThenInclude(x => x.Tag)
+            .Include(x => x.Comments)
+            .ThenInclude(x => x.User)
+            .FirstOrDefault(x => x.Id == id);
             if (article == null)
                 return NotFound();
-            return View();
+            var cookie = _contextAccessor.HttpContext.Request.Cookies[$"Views"];
+            string[] findCookie = { "" }; // ["1","5"]
+            if (cookie != null)
+            {
+                findCookie = cookie.Split("-").ToArray();
+            }
+            if (!findCookie.Contains(article.Id.ToString()))
+            {
+                Response.Cookies.Append($"Views", $"{cookie}-{article.Id}", // 1-2
+                    new CookieOptions
+                    {
+                        Secure = true,
+                        HttpOnly = true,
+                        Expires = DateTime.Now.AddMinutes(1)
+                    }
+                    );
+                article.ViewCount += 1;
+                _context.Articles.Update(article);
+                _context.SaveChanges();
+            }
+            var suggestArticle = _context.Articles.Include(x => x.Category).Where(x => x.Id != article.Id && x.CategoryId == article.CategoryId).Take(2).ToList();
+            var before = _context.Articles.FirstOrDefault(x => x.Id < id);
+            var after = _context.Articles.FirstOrDefault(x => x.Id > id);
+
+            DetailVM detailVM = new()
+            {
+                Article = article,
+                Suggestions = suggestArticle,
+                Befero = before,
+                After = after
+            };
+            return View(detailVM);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Detail(Comment comment)
+        {
+            var userId = _contextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            Comment newComment = new();
+
+            newComment.CreatedDate = DateTime.Now;
+            newComment.UserId = userId;
+            newComment.ArticleId = comment.ArticleId;
+            newComment.Content = comment.Content;
+
+            var article = _context.Articles.FirstOrDefault(x => x.Id == comment.ArticleId);
+            await _context.Comments.AddAsync(newComment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Detail), new { id = article.Id, article.SeoUrl });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
