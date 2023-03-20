238ff09de34fcadfed5824127c3c1f2605ca8f5c
MrAzimzada <mehemmedezimzase@gmail.com>
2023-03-20 19:12:19 +0400

    Add article Videos *

--- WebApp/Components/SectionViewComponent.cs
+++ WebApp/Components/SectionViewComponent.cs
@@ -6,13 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Data;
 using WebApp.ViewModels;
-
 namespace WebApp.Components
 {
     public class SectionViewComponent : ViewComponent
     {
         private readonly AppDbContext _context;
-
         public SectionViewComponent(AppDbContext context)
         {
             _context = context;
@@ -27,8 +25,7 @@ namespace WebApp.Components
                 .AsEnumerable().Where(x => x.IsDelete == false && x.IsActive == true && photoExtensions.Any(ext => x.PhotoUrl.EndsWith(ext)))
                 .Take(5)
                 .ToList();
-
-            var ads = _context.Advertisements.Where(X => X.IsDeleted == false).ToList();
+            var ads = _context.Advertisements.OrderByDescending(x=>x.CreatedDate).Where(X => X.IsDeleted == false).ToList();
             var videoExtensions = new[] { ".mp4", ".avi", ".mov", ".wmv" }; // desteklenen video uzantıları
             var videos = _context.Articles
                 .Include(x => x.Category)
@@ -36,19 +33,17 @@ namespace WebApp.Components
                 .OrderByDescending(x => x.ViewCount)
                 .AsEnumerable() // sorgu sonuçlarını koleksiyona aktar
                 .Where(x => x.IsDelete == false && x.IsActive == true && videoExtensions.Any(ext => x.PhotoUrl.EndsWith(ext))).Take(5).ToList();
-
             SectionVM sectionVm = new()
             {
                 //Articles = articles,
                 //PopularPost = popularPost,
                 PopularSection = popularSection,
                 //Advertisements = ads,
-                Videos = videos
+                Videos = videos,
+                ADS = ads
             };
-
             var viewResult = View(viewName: "Default", model: sectionVm);
             return await Task.FromResult<IViewComponentResult>(viewResult);
-
         }
     }
 }
--- WebApp/ViewModels/SectionVM.cs
+++ WebApp/ViewModels/SectionVM.cs
@@ -7,5 +7,6 @@ namespace WebApp.ViewModels
         public List<Article> Articles { get; set; }
         public List<Article> Videos { get; set; }
         public List<Article> PopularSection { get; set; }
+        public List<Advertisement> ADS { get; set; }
     }
 }
